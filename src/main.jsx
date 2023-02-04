fcb44081023a31980aee97d8e3a96e0f8d7b0b5e
DeiverV <deiberveranodev@gmail.com>
2023-02-04 17:44:04 -0500

    Hotfix import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PortfolioApp from './App'
+import PortfolioApp from './PortfolioApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
